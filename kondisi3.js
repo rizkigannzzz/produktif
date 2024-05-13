a88029cc23a0c04d5da9155de8ae0df21998bc7a
rizkigannzzz <yysshaji@gmail.com>
2024-05-13 14:29:44 +0700

    Create kondisi3.js

---
+++ kondisi3.js
@@ -0,0 +1,38 @@
+//let//
+
+let x= 21 + 14
+ x+= 10 + 11
+console.log(x)
+
+let p= 33 * 21
+ x*= 11 * 15
+console.log(p)
+
+let a= 17 + 25
+ x-= 11 + 15
+console.log(a)
+
+let q= 30 + 27
+ x= 11 + 10
+console.log(q)
+
+let z= 20 - 17
+ x= 11 - 10
+console.log(z)
+
+// var //
+
+var y=10 + 11
+console.log(y)
+
+var y= 5 + 13
+console.log(y)
+
+var y= 10 + 30
+console.log(y)
+
+var y= 22 + 14
+console.log(y)
+
+var y= 11 + 14
+console.log(y
