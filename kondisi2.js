3971930346cc5592e86a546e50343f1a94812873
rizkigannzzz <yysshaji@gmail.com>
2024-05-13 14:28:19 +0700

    Create kondisi2.js

---
+++ kondisi2.js
@@ -0,0 +1,25 @@
+var x="Rizki" ;
+   x+=15;
+  console.log(x)
+  
+var x="Rudi" ;
+  x+=15;
+  console.log(x)
+  
+var x="78" ;
+   x+=15;
+  console.log(x)
+  
+var x="Semarang" ;
+   x+=1889;
+  console.log(x)
+  
+var x="Blora" ;
+   x+=78;
+  console.log(x)
+
+var y={nama:"rizki hibatul wafi",umur:16,absen:29,kelas:"X rpl 3"}
+console.log(y.nama,y.umur,y.absen,y.kelas)
+
+i=5
+console.log(++i);
